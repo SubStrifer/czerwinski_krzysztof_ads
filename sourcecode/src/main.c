a0806b1d415910475479555daaa1346bb66b4074
Krzysztof Czerwinski <substrifer@gmail.com>
2019-02-22 18:40:02 +0000

    Added test files

---
+++ sourcecode/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <time.h>
+#include "..\inc\game.h"
+
+int testValue;
+
+int main(int argc, char **argv)
+{
+	testValue = 774;
+
+	printf("Hello World! %d", testValue);
+}
